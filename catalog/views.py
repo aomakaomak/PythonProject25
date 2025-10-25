0675d8f14a8f0ffafee732bbf48479131d58a548
Andrei Makarov <aomak@yandex.ru>
2025-10-25 18:10:37 +0300

    задача 1 готово

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from catalog.models import Product
+
 # Create your views here.
 
 def home(request):
@@ -14,6 +16,16 @@ def contacts(request):
         message = request.POST.get('message')
         return HttpResponse(f'{name}, спасибо за сообщение')
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'catalog/product_detail.html', context=context)
+
+
 
 
 
