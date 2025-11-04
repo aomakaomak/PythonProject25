b39caa6da7259b6c996e5d4c390b3562d6414104
Andrei Makarov <aomak@yandex.ru>
2025-11-04 22:11:49 +0300

    задание 3 готово

--- catalog/forms.py
+++ catalog/forms.py
@@ -9,6 +9,30 @@ class ProductForm(forms.ModelForm):
         fields = ['name', 'description', 'category', 'price']
 
 
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите название',
+        })
+
+        self.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите описание',
+        })
+
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Выберете категорию',
+        })
+
+        self.fields['price'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите цену',
+        })
+
+
     def clean_name(self):
 
         forbidden_words = {
@@ -34,3 +58,12 @@ class ProductForm(forms.ModelForm):
             if word in description.lower():
                 raise ValidationError('Запрещенное слово в описании товара!')
         return description
+
+
+    def clean_price(self):
+
+        price = self.cleaned_data.get('price')
+
+        if price < 0:
+            raise ValidationError('Цена не может быть отрицательной!')
+        return price
