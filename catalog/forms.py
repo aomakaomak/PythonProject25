93c9df357de7b8e6088daa0572595f2fa9664456
Andrei Makarov <aomak@yandex.ru>
2025-11-04 21:41:49 +0300

    задание 1 готово

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,7 +10,27 @@ class ProductForm(forms.ModelForm):
 
 
     def clean_name(self):
+
+        forbidden_words = {
+            'казино', 'криптовалюта', 'крипта', 'биржа',
+            'дешево', 'бесплатно', 'обман', 'полиция', 'радар'
+        }
         name = self.cleaned_data.get('name')
-        if not email.endswith('@example.com'):
-            raise ValidationError('Email должен оканчиваться на @example.com')
-        return email
+
+        for word in forbidden_words:
+            if word in name.lower():
+                raise ValidationError('Запрещенное слово в названии товара!')
+        return name
+
+    def clean_description(self):
+
+        forbidden_words = {
+            'казино', 'криптовалюта', 'крипта', 'биржа',
+            'дешево', 'бесплатно', 'обман', 'полиция', 'радар'
+        }
+        description = self.cleaned_data.get('description')
+
+        for word in forbidden_words:
+            if word in description.lower():
+                raise ValidationError('Запрещенное слово в описании товара!')
+        return description
